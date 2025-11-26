8d0f7e4307fd605d2f6be1647b2e4ba2421eb73b
khalidelaasry <khalidelaasry@gmail.com>
2025-11-26 19:58:36 +0100

    :|

--- ft_printf.h
+++ ft_printf.h
@@ -11,21 +11,21 @@
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <stdint.h>
-#include <unistd.h>
+# include <stdarg.h>
+# include <stdint.h>
+# include <unistd.h>
 
-int ft_printf(const char *format, ...);
+int	ft_printf(const char *format, ...);
 
-int ft_putchar(int c);
-int ft_putstr(char *s);
-int ft_putnbr(int n);
-int ft_putunsigned(unsigned int n);
-int ft_puthex(unsigned int n, char spec);
-int ft_putptr(void *ptr);
+int	ft_putchar(int c);
+int	ft_putstr(char *s);
+int	ft_putnbr(int n);
+int	ft_putunsigned(unsigned int n);
+int	ft_puthex(unsigned int n, char spec);
+int	ft_putptr(void *ptr);
 
-int ft_conversion(va_list args, const char specifier);
+int	ft_conversion(va_list args, const char specifier);
 
 #endif
